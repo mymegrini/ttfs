f53d3a214c6299a2a60f0a62ad671b2a9bfd3449
nscott32 <scottodn@hotmail.fr>
2015-12-01 21:30:03 +0100

    disk_ent and __o_disk added to lib/ll.c

--- lib/ll.c
+++ lib/ll.c
@@ -1,6 +1,22 @@
 #include "ll.h"
 #include "error.h"
 
+
+
+/**                                                                                                                                            
+ * A structure containing informations about an opened disk                                                                                    
+ */
+typedef struct disk_ent =
+  {
+    char *name;      /**< name of the disk */
+    int fd;          /**< file descriptor */
+    uint32_t size;   /**< size of the disk */
+
+  } disk_ent;
+
+extern disk_ent __o_disk[DD_MAX];    /**< opened disks. A disk_id refers to an index in this array */
+
+
 /**
  * 
  * 
