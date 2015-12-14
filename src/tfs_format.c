76968ca0c4a437af9baf19a3f34683e6a03990f0
Younes M <rasetoi@yahoo.fr>
2015-12-14 11:51:04 +0100

    creating filesystem in progress

--- src/tfs_format.c
+++ src/tfs_format.c
@@ -7,10 +7,9 @@
 #include <stdlib.h>
 
 #define DEF_NAME "disk.tfs"   /***< default disk name */
-
-#ifndef D_NAME_MAXLEN
+#define F_OWR 1    /***< flag for overwrite option */
+#define MAGIC_NUMBER 0x31534654 /***< magic number */
 #define D_NAME_MAXLEN 79     /***< disk name maximum length */
-#endif
 
 /**
  * @brief This command creates a minimal filesystem
@@ -32,6 +31,7 @@ int main(int argc, char* argv[]){
   int partition = -1;
   int filecount = -1;
   char name[D_NAME_MAXLEN+1];
+  int flags = 0;
   
   while (1) {
     int index = 0;
@@ -39,9 +39,10 @@ int main(int argc, char* argv[]){
       {"help", no_argument, NULL, 'h'},
       {"partition",  required_argument, NULL, 'p'},
       {"mf",  required_argument, NULL, 'f'},
+      {"overwrite",  no_argument, NULL, 'o'},
       {0, 0, 0, 0}
     };
-    c = getopt_long_only(argc, argv, "p:f:",
+    c = getopt_long_only(argc, argv, "p:f:o",
 			 long_options, &index);
     
     if (c==-1) break;
@@ -53,6 +54,7 @@ existing partition\n\
 Usage: %s -p <partition> -mf <max-file-count> [<name>]\n\n\
   -p\t--partition\tspecify partition id\n\
   -f\t-mf\t\tspecify maximum file count for the file system\n\
+  -o\t--overwrite\toverwrite existing disk\n\
     \t--help\t\tdisplay this help and exit\n\n",
 	     argv[0]);
       exit(EXIT_SUCCESS);
@@ -72,6 +74,9 @@ Usage: %s -p <partition> -mf <max-file-count> [<name>]\n\n\
 	fprintf(stderr, "Usage: %s -p <partition> -mf <max-file-count> [<name>]\n",argv[0]);
 	exit(C_FORMAT);
       }
+    case 'o':
+      flags |= F_OWR;
+      break;
     default: /* '?' */
       fprintf(stderr, "Usage: %s -p <partition> -mf <max-file-count> [<name>]\n",argv[0]);
       exit(C_FORMAT);
@@ -87,18 +92,100 @@ Usage: %s -p <partition> -mf <max-file-count> [<name>]\n\n\
     }
   } else strncpy(name, DEF_NAME, 9);
 
-  if (partition==-1 || filecount==-1){
-    fprintf(stderr, "Usage: %s -p <partition> -mf <max-file-count> [<name>]\n",argv[0]);
+  if (partition<0 || filecount<1){
+    fprintf(stderr,
+	    "%s: requires positive <partition> and strictly positive <max-file-count> values\n\
+Usage: %s -p <partition> -mf <max-file-count> [<name>]\n"
+	    ,argv[0],argv[0]);
     exit(C_FORMAT);
   } else {
     disk_id id;
     error err;
+    d_stat stat;
     
     err = start_disk(name, &id);    
     if(err!=EXIT_SUCCESS){
       printerror("start_disk", err);
       exit(err);
     }
-    
+
+    err = disk_stat(id, &stat);    
+    if(err!=EXIT_SUCCESS){
+      printerror("disk_stat", err);
+      exit(err);
+    }
+
+    if (partition<0 || partition >D_PARTMAX-1 || partition>stat.npart-1){
+      printerror(argv[0], P_WRONGIDX);
+      exit(P_WRONGIDX);
+    } else {
+      uint32_t psize;
+      uint32_t pidx =1;
+      block b = new_block();
+      uint32_t k;
+      
+      psize = stat.part[partition];
+      for(k=0; k<partition; k++) pidx+=stat.part[k];
+
+      err = read_block(id, b, pidx);    
+      if(err!=EXIT_SUCCESS){
+	printerror("read_block", err);
+	exit(err);
+      }
+
+      err = rintle(&k, b, 0);    
+      if(err!=EXIT_SUCCESS){
+	printerror("rintle 0", err);
+	exit(err);
+      }
+
+      if((flags & F_OWR)==0 && k == 0x31534654){
+	char answer = 0;
+	while(answer == 0){
+	  printf("%s: partition %d already contains a filesystem. Overwrite? [Y/n] ",
+		 argv[0], partition);
+	  answer = getchar();
+	  switch(answer){
+	  case 'Y':	
+	  case 'y':
+	    break;
+	  case 'n':
+	  case 'N':
+	    exit(EXIT_SUCCESS);
+	  default:
+	    answer = 0;
+	  }
+	}
+      }
+
+      free(b);
+      b=new_block();
+
+      err = wrintle(MAGIC_NUMBER, b, 0);    
+      if(err!=EXIT_SUCCESS){
+	printerror("wintle 0", err);
+	exit(err);
+      }
+
+      err = wrintle(B_SIZE, b, 4);    
+      if(err!=EXIT_SUCCESS){
+	printerror("wintle 4", err);
+	exit(err);
+      }
+      
+      err = wrintle(psize, b, 8);    
+      if(err!=EXIT_SUCCESS){
+	printerror("wintle 0", err);
+	exit(err);
+      }
+
+      
+      err = wrintle(psize, b, 8);    
+      if(err!=EXIT_SUCCESS){
+	printerror("wintle 0", err);
+	exit(err);
+      }
+
+    }    
   }
 }
