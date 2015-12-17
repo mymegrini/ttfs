d1ec479ba251448fbf0e53369eb2b0b810d395eb
Younes M <rasetoi@yahoo.fr>
2015-12-17 14:24:20 +0100

    tfs_create rewritten for clarity and modularity

--- src/tfs_create.c
+++ src/tfs_create.c
@@ -15,12 +15,6 @@
 #define D_NAME_MAXLEN 79     /***< disk name maximum length */
 
 /**
- * @brief This command creates and initializes a new disk
- * @param[in] name disk name
- * @param[in] size disk size
- * @param[out] id wheere disk id is stored
- * @return Returns an error if encountered
- *
  * This command creates and initializes a new disk.
  * It allocates enough memory for <size> blocks
  * and stores <size> in the first block
@@ -31,13 +25,99 @@
  * @see EXIT_SUCCESS
  */
 
+/**
+ * @brief This function waits for an answer from the user
+ * @param[in] prompt message for the user
+ * @return Returns 0 for [n/N] and 1 for [y/Y]
+ *
+ * This function displays a prompt message <prompt> and
+ * waits for the user's answer and returns it
+ */
+int answer(char* prompt){
+  fpurge(stdin);
+  printf("%s", prompt);
+  switch(getchar()){
+  case 'Y':	
+  case 'y':
+    return 1;
+  case 'n':
+  case 'N':
+    return 0;
+  default:
+    return answer(prompt);
+  }
+}
+
+/**
+ * @brief This function creates and initializes a disk
+ * @param[in] name containing path for the disk
+ * @param[in] size of the disk
+ * @param[in] argv0 name of the calling command
+ * @return void
+ *
+ * This function creates a disk <name> of size <size>
+ * and fills in the appropriate values in the description
+ * block
+ *
+ * @see C_FORMAT
+ * @see D_OPEN_ERR
+ * @see D_SEEK_ERR
+ * @see EXIT_SUCCESS
+ */
+void create_disk(char* name, int size, char* argv0){
+  int disk;
+  block b = new_block();   // block filled with 0 bytes
+
+  if ((disk = open(name,O_WRONLY|O_CREAT|O_TRUNC,S_IRUSR|S_IWUSR))==-1){
+    printerror(argv0, D_OPEN_ERR);
+    exit(D_OPEN_ERR);
+  } else {    
+    if (lseek(disk, (size-1)*B_SIZE, SEEK_SET)==-1){
+      printerror(argv0, D_SEEK_ERR);
+      exit(D_SEEK_ERR);
+    }    
+    if (write(disk, b, B_SIZE)==-1){
+      printerror(argv0, D_OPEN_ERR);
+      exit(D_OPEN_ERR);
+    }
+    
+    wintle(size, b, 0);
+    wintle(0, b, 4);
+    
+    if (lseek(disk, 0, SEEK_SET)==-1){
+      printerror(argv0, D_SEEK_ERR);
+      exit(D_SEEK_ERR);
+    }
+    if (write(disk, b, B_SIZE)==-1){
+      printerror(argv0, D_OPEN_ERR);
+      exit(D_OPEN_ERR);
+    }    
+    free(b);  //free block
+    close(disk);
+    printf("%s: %dB\n",name,size*B_SIZE);
+    exit(EXIT_SUCCESS);
+  }
+}
+
+/**
+ * @brief This function parses command line inputs and
+ * launches auxiliary functions
+ * @param[in] argc number of command line inputs
+ * @param[in] argv array of command line inputs
+ * @return int value storing the error encountered
+ *
+ * This function parses command line inputs and options
+ * then launches the auxiliary function create_disk to 
+ * create the disk with the appropriate parameters
+ *
+ * @see create_disk
+ * @see answer
+ */
 int main(int argc, char* argv[]){
-   int c;
+  int c;
   int size = -1;
   int flags = 0;
   char name[D_NAME_MAXLEN+1];
-  int disk;
-  block b = new_block();   // block filled with 0 bytes
   
   while (1) {
     int index = 0;
@@ -103,51 +183,10 @@ Usage: %s [-o] -s <size> [<name>]\n",
   } else strncpy(name, DEF_NAME, 9);
 
   if ((flags & F_OWR)==0 && access(name, F_OK)==0){
-    char answer = 0;
-    while(answer == 0){
-      printf("%s: disk '%s' already exists. Overwrite? [Y/n] ",
-	     argv[0], name);
-      answer = getchar();
-      switch(answer){
-      case 'Y':	
-      case 'y':
-	break;
-      case 'n':
-      case 'N':
-	exit(EXIT_SUCCESS);
-      default:
-	answer = 0;
-      }
-    }
-  }
-  
-  if ((disk = open(name,O_WRONLY|O_CREAT|O_TRUNC,S_IRUSR|S_IWUSR))==-1){
-    printerror(argv[0], D_OPEN_ERR);
-    exit(D_OPEN_ERR);
-  } else {    
-    if (lseek(disk, (size-1)*B_SIZE, SEEK_SET)==-1){
-      printerror(argv[0], D_SEEK_ERR);
-      exit(D_SEEK_ERR);
-    }    
-    if (write(disk, b, B_SIZE)==-1){
-	printerror(argv[0], D_OPEN_ERR);
-	exit(D_OPEN_ERR);
-    }
-    
-    wintle(size, b, 0);
-    wintle(0, b, 4);
-    
-    if (lseek(disk, 0, SEEK_SET)==-1){
-      printerror(argv[0], D_SEEK_ERR);
-      exit(D_SEEK_ERR);
-    }
-    if (write(disk, b, B_SIZE)==-1){
-	printerror(argv[0], D_OPEN_ERR);
-	exit(D_OPEN_ERR);
-    }    
-    free(b);  //free block
-    close(disk);
-    printf("%s: %dB\n",name,size*B_SIZE);
-    exit(EXIT_SUCCESS);
+    printf("%s: disk '%s' already exists.\n",
+	   argv[0], name);
+    if (answer("Overwrite? [Y/n] "))
+      create_disk(name, size, argv[0]);
+    else exit(EXIT_SUCCESS);
   }
 }
