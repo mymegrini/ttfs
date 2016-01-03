95c338479e118d5761bb8ea1d3ac07b50cfa30c7
nscott32 <scottodn@hotmail.fr>
2016-01-03 16:42:01 +0100

    tfs low level source file skeleton

---
+++ lib/tfsll.c
@@ -0,0 +1,76 @@
+// tfsll.c
+//
+// last-edit-by: <nscott32> 
+// 
+// Description: TFS low level library source code
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "tfsll.h"
+
+
+error
+freeblock_push ( const uint32_t b_addr ) {
+
+  return EXIT_SUCCESS;
+}
+
+
+
+error
+freeblock_rm ( const uint32_t b_addr ) {
+
+  return EXIT_SUCCESS;
+}
+
+
+
+error
+directory_pushent ( DIR directory, const struct dirent *restrict entry  ) {
+
+  return EXIT_SUCCESS;
+}
+
+
+
+error
+directory_rment ( DIR directory, const struct dirent *restrict entry ) {
+
+  return EXIT_SUCCESS;
+}
+
+
+
+error
+file_pushblock ( uint32_t inode, uint32_t b_addr ) {
+
+  return EXIT_SUCCESS;
+}
+
+
+
+error
+file_rmblock( uint32_t inode, uint32_t b_addr ) {
+
+  return EXIT_SUCCESS;
+}
+
+
+
+error
+file_freeblocks ( uint32_t inode ) {
+
+  return EXIT_SUCCESS;
+}
+
+
+
+error
+path_follow ( char * path, char * entry ) {
+
+  return EXIT_SUCCESS;
+}
+
+//////////////////////////////////////////////////////////////////////
+// $Log:$
+//
